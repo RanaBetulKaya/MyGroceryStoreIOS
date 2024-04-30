2cb7fd63e9c00dc0896a34c2a6c6f321c5c2282f
RanaBetulKaya <rana20032016@icloud.com>
2024-04-30 23:59:10 +0300

    Campaign Collection view cells has been created

---
+++ MyGroceryStoreIOS/CollectionViewCells/CampaignViewCell/CampaignCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CampaignCollectionViewCell.swift
+//  MyGroceryStoreIOS
+//
+//  Created by Rana Betül Kaya on 29.04.2024.
+//
+
+import UIKit
+
+class CampaignCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
