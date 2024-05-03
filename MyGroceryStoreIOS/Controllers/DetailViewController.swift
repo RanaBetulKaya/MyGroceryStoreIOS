313d0fa11604a819f762d7c0e1ea67bc4642eb05
RanaBetulKaya <rana20032016@icloud.com>
2024-05-03 22:50:48 +0300

    detail view controller has been fixed

--- MyGroceryStoreIOS/Controllers/DetailViewController.swift
+++ MyGroceryStoreIOS/Controllers/DetailViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var detailImageView: UIImageView!
+    
+    @IBOutlet weak var detailPriceLabel: UILabel!
+    
+    @IBOutlet weak var detailDescTextView: UITextView!
+
+    var product: SaleProductModel!
+    
     override func viewDidLoad() {
+        print(product)
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func detailCommentBtn(_ sender: UIButton) {
     }
-    */
-
+    
 }
