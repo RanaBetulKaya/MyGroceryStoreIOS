ce2d2f4f211e6271c4e954f5bca3f5794c76172b
RanaBetulKaya <rana20032016@icloud.com>
2024-05-03 22:34:29 +0300

    categorylist viewcell has been created

---
+++ MyGroceryStoreIOS/CollectionViewCells/CategoryListViewCell/CategoryListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryListCollectionViewCell.swift
+//  MyGroceryStoreIOS
+//
+//  Created by Rana Betül Kaya on 1.05.2024.
+//
+
+import UIKit
+
+class CategoryListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
