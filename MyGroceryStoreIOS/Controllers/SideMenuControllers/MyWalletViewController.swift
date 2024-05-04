d26b7fdc32ccff6fcb281c80029e112d52b91873
RanaBetulKaya <rana20032016@icloud.com>
2024-05-04 20:33:41 +0300

    my wallet screen has been created, user can add & view money

--- MyGroceryStoreIOS/Controllers/SideMenuControllers/MyWalletViewController.swift
+++ MyGroceryStoreIOS/Controllers/SideMenuControllers/MyWalletViewController.swift
@@ -6,16 +6,104 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class MyWalletViewController: UIViewController {
     
+    var ref: DatabaseReference!
+    
+    
+    @IBOutlet weak var viewBudgetLabel: UILabel!
+    @IBOutlet weak var addingAmountInput: UITextField!
     @IBOutlet weak var sideMenuBtn: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            // Do any additional setup after loading the view.
+        ref = Database.database().reference(fromURL: "https://my-grocery-store-c4018-default-rtdb.europe-west1.firebasedatabase.app/")
+        
         sideMenuBtn.target = revealViewController()
         sideMenuBtn.action = #selector(revealViewController()?.revealSideMenu)
+        
+        viewBudget()
+        self.viewBudgetLabel.reloadInputViews()
     }
 
+    /*func addMoney(){
+        
+        guard let addingAmount = addingAmountInput.text else{
+            return
+        }
+        var intAddingAmount: Int = Int(addingAmount)!
+        var budget: Int = 0
+        ref.child("Users").child(Auth.auth().currentUser!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            if let userData = snapshot.value as? [String: Any] {
+                // Kullanıcı verilerini al
+                budget = userData["budget"] as! Int
+                
+            }
+            }) { (error) in
+                print("Firebase veri alımı başarısız: \(error.localizedDescription)")
+            }
+        var newBudget = budget + intAddingAmount
+        
+        let userData = ["budget": newBudget]
+        
+        ref.child("Users").child(Auth.auth().currentUser!.uid).updateChildValues(userData) { (error, ref) in
+                if let error = error {
+                    print("Firebase veri güncelleme hatası: \(error.localizedDescription)")
+                } else {
+                    print("Kullanıcı bilgileri güncellendi")
+                }
+            }
+        
+    }*/
+    func addMoney() {
+        guard let addingAmount = addingAmountInput.text,
+              let intAddingAmount = Int(addingAmount) else {
+            return
+        }
+        
+        ref.child("Users").child(Auth.auth().currentUser!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            if let userData = snapshot.value as? [String: Any] {
+                // Kullanıcı verilerini al
+                if let budget = userData["budget"] as? Int {
+                    let newBudget = budget + intAddingAmount
+                    let userData = ["budget": newBudget]
+                    
+                    self.ref.child("Users").child(Auth.auth().currentUser!.uid).updateChildValues(userData) { (error, ref) in
+                        if let error = error {
+                            print("Firebase veri güncelleme hatası: \(error.localizedDescription)")
+                        } else {
+                            print("Kullanıcı bilgileri güncellendi")
+                        }
+                    }
+                }
+            }
+        }) { (error) in
+            print("Firebase veri alımı başarısız: \(error.localizedDescription)")
+        }
+    }
+
+    func viewBudget(){
+        ref.child("Users").child(Auth.auth().currentUser!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            if let userData = snapshot.value as? [String: Any] {
+                
+                let budget: Int = userData["budget"] as? Int ?? 0
+                
+                let stringBudget: String = String(budget)
+                
+                self.viewBudgetLabel.text = stringBudget + "₺"
+                
+            }
+            }) { (error) in
+                print("Firebase veri alımı başarısız: \(error.localizedDescription)")
+            }
+    }
+    
+    @IBAction func addMoneyBtn(_ sender: Any) {
+        addMoney()
+        self.viewBudgetLabel.reloadInputViews()
+    }
+    
 }
