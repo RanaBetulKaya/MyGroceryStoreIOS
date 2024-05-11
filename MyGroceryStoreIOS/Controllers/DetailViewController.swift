0fd9a703b538282ebe839e265f33c40f7a0a81df
RanaBetulKaya <rana20032016@icloud.com>
2024-05-11 16:13:23 +0300

    design has been created for detailView

--- MyGroceryStoreIOS/Controllers/DetailViewController.swift
+++ MyGroceryStoreIOS/Controllers/DetailViewController.swift
@@ -11,10 +11,16 @@ class DetailViewController: UIViewController {
 
     var product: SaleProductModel!
     
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var detailPriceLbl: UILabel!
+    @IBOutlet weak var detailDescTextField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print(product.name)
+        detailImageView.kf.setImage(with: product.img_url?.asURL)
+        detailPriceLbl.text = product.formattedPrice + " ₺"
+        detailDescTextField.text = product.description
+        //print(product.name)
         print("Detail girdi")
         
         // Do any additional setup after loading the view.
