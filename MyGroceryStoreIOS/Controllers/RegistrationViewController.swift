8376385004383a0b43aac6d6f3cfb4629f7db43d
RanaBetulKaya <rana20032016@icloud.com>
2024-05-19 15:06:51 +0300

    registration has been fixed user data added to db

--- MyGroceryStoreIOS/Controllers/RegistrationViewController.swift
+++ MyGroceryStoreIOS/Controllers/RegistrationViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import FirebaseDatabase
 
 class RegistrationViewController: UIViewController {
 
@@ -16,6 +17,9 @@ class RegistrationViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var userName: UITextField!
+    
+    var ref: DatabaseReference!
+    
     @IBAction func signUp(_ sender: Any) {
         if email.text?.isEmpty == true{
             print("No text in email field")
@@ -37,6 +41,8 @@ class RegistrationViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference(fromURL: "https://my-grocery-store-c4018-default-rtdb.europe-west1.firebasedatabase.app/")
     }
     func signUp(){
         Auth.auth().createUser(withEmail: email.text!, password: password.text!){
@@ -44,11 +50,31 @@ class RegistrationViewController: UIViewController {
                 print("Error \(error?.localizedDescription)")
                 return
             }
+            
+            if(self.email.text==" "){
+                self.showAlertMessage(title: "HATA!", message: "Email adresini boş bırakamazsınız.")
+            }
+            if(self.password.text==" "){
+                self.showAlertMessage(title: "HATA!", message: "Şifre alanını boş bırakamazsınız.")
+            }
+            else{
+                let userData=["address":self.address.text,
+                              "budget":0,
+                              "mail":self.email.text,
+                              "name":self.userName.text,
+                              "password":self.password.text,
+                              "phone":self.telephone.text]
+                let userRef = self.ref.child("Users").child(Auth.auth().currentUser!.uid)
+                userRef.setValue(userData){ error, _ in
+                    if let error = error {
+                        print("Veri eklenirken hata oluştu: \(error.localizedDescription)")
+                    } else {
+                        print("Kullanıcı verisi başarıyla eklendi")
+                        self.showAlertMessage(title: "Kayıt Alındı", message: "Kaydınız alındı. Sisteme giriş yapabilirsiniz.")
+                    }
+                }
+            }
         }
-        let storyboard=UIStoryboard(name: "Main", bundle: nil)
-        let vc=storyboard.instantiateViewController(identifier: "signInView")
-        vc.modalPresentationStyle = .overFullScreen
-        present(vc,animated: true)
     }
 
 }
